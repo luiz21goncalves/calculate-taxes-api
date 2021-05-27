ceb285edf510d86167e033298baa7b395f3ad191
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-27 19:43:23 -0300

    feat: add xml2json & get fields to calculate taxes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import convert from 'xml-js'
+import convert from 'xml2json'
 
 const basePath = path.resolve(__dirname, '../tmp')
 
@@ -10,14 +10,25 @@ const basePath = path.resolve(__dirname, '../tmp')
 //   files.forEach(file => {
 //     const value = fs.readFileSync(path.join(basePath, `/xml/${file}`)).toString()
     
-//     const json = convert.xml2json(value, {compact: true, ignoreComment: true, spaces: 2})
+//     const json = convert.toJson(value)
 
 //     fs.writeFileSync(path.resolve(
 //       __dirname, 
 //       `../tmp/json/${file.replace('xml', 'json')}`),
-//        json
-//     )
+//       json
+//       )
 //   })
+// })
+
+// fs.readdir(path.join(basePath, 'json'), (err, files) => {
+//   if (err) return console.error(err)
+
+//   files.forEach(file => {
+//     const json = require(`../tmp/json/${file}`)
+ 
+//     fs.writeFileSync(path.resolve(__dirname, `../tmp/json/${file}`), JSON.stringify(json,  null, 4))
+//   })
+
 // })
 
 fs.readdir(path.resolve(path.join(basePath, 'json')), (err, files) => {
@@ -26,58 +37,58 @@ fs.readdir(path.resolve(path.join(basePath, 'json')), (err, files) => {
   files.forEach((file, index) => {
     const json = require(`../tmp/json/${file}`)
 
-    const number = json.nfeProc.NFe.infNFe.ide.nNF._text
+    const number = json.nfeProc.NFe.infNFe.ide.nNF
     const company = {
-      cnpj: json.nfeProc.NFe.infNFe.emit.CNPJ._text,
-      name: json.nfeProc.NFe.infNFe.emit.xNome._text
+      cnpj: json.nfeProc.NFe.infNFe.emit.CNPJ,
+      name: json.nfeProc.NFe.infNFe.emit.xNome
     }
     const customer = {
-      cnpj: json.nfeProc.NFe.infNFe.dest.CNPJ._text,
-      name: json.nfeProc.NFe.infNFe.dest.xNome._text
+      cnpj: json.nfeProc.NFe.infNFe.dest.CNPJ,
+      name: json.nfeProc.NFe.infNFe.dest.xNome
     }
 
-    let isNotProductsArray = json.nfeProc.NFe.infNFe.det._attributes?.nItem
     let products = []
+    const isNotProductArray = json.nfeProc.NFe.infNFe.det.prod
 
-    if (isNotProductsArray) {
+    if (isNotProductArray) {
       products.push({
-        index: Number(json.nfeProc.NFe.infNFe.det._attributes.nItem),
-        name: json.nfeProc.NFe.infNFe.det.prod.xProd._text,
-        quantity: Number(json.nfeProc.NFe.infNFe.det.prod.qCom._text),
-        priceUnit: Number(json.nfeProc.NFe.infNFe.det.prod.vUnCom._text),
-        total: Number(json.nfeProc.NFe.infNFe.det.prod.vProd._text),
-        taxes: {
-          icmsST: json.nfeProc.NFe.infNFe.det.imposto.ICMS
-        }
+        name: json.nfeProc.NFe.infNFe.det.prod.xProd,
+        quantity: json.nfeProc.NFe.infNFe.det.prod.qCom,
+        price: json.nfeProc.NFe.infNFe.det.prod.vProd,
+        other: json.nfeProc.NFe.infNFe.det.prod.vOutro,
+        icmsSt: json.nfeProc.NFe.infNFe.det.imposto?.ICMS[Object.keys(json.nfeProc.NFe.infNFe.det.imposto.ICMS)[0]].vICMSST,
       })
     }
 
-    if (!isNotProductsArray) {
-      products = json.nfeProc.NFe.infNFe.det.map(item => {
-        return {
-          index: Number(item._attributes.nItem),
-          name: item.prod.xProd._text,
-          quantity: Number(item.prod.qCom._text),
-          priceUnit: Number(item.prod.vUnCom._text),
-          total: Number(item.prod.vProd._text),
-          taxes: {
-            icmsST: Array.from(item.imposto.ICMS).map(item => item)
-          }
-        }
-      })     
+    if (!isNotProductArray) {
+      json.nfeProc.NFe.infNFe.det.forEach(element => {
+        products.push({
+          name: element.prod.xProd,
+          quantity: element.prod.qCom,
+          price: element.prod.vProd,
+          other: element.prod.vOutro,
+          icmsSt: element.imposto?.ICMS[Object.keys(element.imposto.ICMS)[0]].vICMSST,
+        })
+      });
     }
 
     const total = {
-      products: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vProd._text),
-      others: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vOutro._text),
-      st: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vST._text),
-      shipping: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vFrete._text),
-      ipi: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vIPI._text),
-      discount: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vDesc._text),
-      safe: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vSeg._text),
-      nf: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vNF._text)
+      products: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vProd),
+      others: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vOutro),
+      st: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vST),
+      shipping: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vFrete),
+      ipi: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vIPI),
+      discount: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vDesc),
+      safe: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vSeg),
+      nf: Number(json.nfeProc.NFe.infNFe.total.ICMSTot.vNF)
     }
 
-    console.log({index, products, total })
+    console.log({
+      number,
+      company,
+      customer,
+      products,
+      total
+    })
   })
 })
