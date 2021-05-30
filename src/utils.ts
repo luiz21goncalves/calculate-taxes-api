8cda3fa1cad38d359eed17a148a13268e4c8c1d9
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-30 07:35:20 -0300

    feat: add type to products and total

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,31 @@ import fs from 'fs';
 import { resolve } from 'path';
 import convert from 'xml2json'
 
+type IProducts = {
+  id: number;
+  name: string;
+  quantity: number;
+  unit_price: number;
+  total_price: number;
+  discount: number;
+  taxes: {
+    ipi: number ;
+    icms_st: number;
+  }
+  other: number;
+}
+
+type ITotal = {
+  products: number;
+  others: number;
+  ipi: number;
+  icms_st: number;
+  shipping: number;
+  safe: number;
+  discount: number;
+  nf: number;
+}
+
 export const convertToJson = (path: string, filename: string) => {
   const value = fs.readFileSync(path).toString()
 
@@ -13,7 +38,7 @@ export const convertToJson = (path: string, filename: string) => {
 export const getFields = (filename) => {
   const json = require(`../tmp/json/${filename}.json`)
 
-  const number = json.nfeProc.NFe.infNFe.ide.nNF
+  const number = Number(json.nfeProc.NFe.infNFe.ide.nNF)
     const company = {
       cnpj: json.nfeProc.NFe.infNFe.emit.CNPJ,
       name: json.nfeProc.NFe.infNFe.emit.xNome
@@ -23,14 +48,20 @@ export const getFields = (filename) => {
       name: json.nfeProc.NFe.infNFe.dest.xNome
     }
 
-    let products = []
+    let products: IProducts[] = []
     const isNotProductArray = json.nfeProc.NFe.infNFe.det.prod
 
     if (isNotProductArray) {
       products.push({
-        name: json.nfeProc.NFe.infNFe.det.prod.xProd,
+        id: Number(json.nfeProc.NFe.infNFe.det.prod.cProd),
+        name: String(json.nfeProc.NFe.infNFe.det.prod.xProd),
         quantity: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.qCom),
-        price: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.vProd),
+        unit_price: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.vUnCom),
+        total_price: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.vProd),
+        taxes: {
+          icms_st: serializeNumbers(json.nfeProc.NFe.infNFe.det.imposto?.ICMS[Object.keys(json.nfeProc.NFe.infNFe.det.imposto.ICMS)[0]].vICMSST),
+          ipi: serializeNumbers(json.nfeProc.NFe.infNFe.det.imposto?.IPI?.IPITrib?.vIPI)
+        },
         other: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.vOutro),
         icmsSt: serializeNumbers(json.nfeProc.NFe.infNFe.det.imposto?.ICMS[Object.keys(json.nfeProc.NFe.infNFe.det.imposto.ICMS)[0]].vICMSST),
         discount: serializeNumbers(json.nfeProc.NFe.infNFe.det.prod.vDesc),
@@ -38,7 +69,7 @@ export const getFields = (filename) => {
     }
 
     if (!isNotProductArray) {
-      json.nfeProc.NFe.infNFe.det.forEach(element => {
+      json.nfeProc.NFe.infNFe.det.forEach(product => {
         products.push({
           name: element.prod.xProd,
           quantity: serializeNumbers(element.prod.qCom),
@@ -53,13 +84,13 @@ export const getFields = (filename) => {
     const total = {
       products: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vProd),
       others: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vOutro),
-      st: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vST),
+      icms_st: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vST),
       shipping: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vFrete),
       ipi: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vIPI),
       discount: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vDesc),
       safe: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vSeg),
       nf: serializeNumbers(json.nfeProc.NFe.infNFe.total.ICMSTot.vNF)
-    }
+    } as ITotal;
 
     fs.unlinkSync(resolve(__dirname, '..','tmp','json', `${filename}.json` ))
     fs.unlinkSync(resolve(__dirname, '..','tmp','xml', `${filename}.xml` ))
