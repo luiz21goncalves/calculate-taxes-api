4662a2c41cb289e5f1674dcdd58aa38008e8461a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-08-01 10:17:18 -0300

    refactor: isolate upload config

---
+++ src/config/upload.ts
@@ -0,0 +1,21 @@
+import { randomBytes } from 'crypto';
+import multer, { Options } from 'multer';
+import { resolve, join } from 'path';
+
+const tmpFolder = resolve(__dirname, '..', 'tmp');
+
+const uploadConfig: Options = {
+  storage: multer.diskStorage({
+    destination: (request, file, callback) => {
+      callback(null, join(tmpFolder, 'xml'));
+    },
+
+    filename: (request, file, callback) => {
+      const hash = randomBytes(12).toString('hex');
+      const filename = `${hash}-${file.originalname}`;
+      callback(null, filename);
+    },
+  }),
+};
+
+export { uploadConfig, tmpFolder };
