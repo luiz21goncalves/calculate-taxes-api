b02957b739c07d2ee8b098f36c0fe4e76fb9705a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-22 20:40:33 -0300

    chore: add logger

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,16 @@ import cors from '@fastify/cors'
 import fastify from 'fastify'
 import healthcheck from 'fastify-healthcheck'
 
-const app = fastify()
+import { logger } from './logger'
+
+const app = fastify({ logger })
 
 app.register(cors)
 app.register(healthcheck, { exposeUptime: true })
 
-app.setErrorHandler((_error, _request, replay) => {
+app.setErrorHandler((error, _request, replay) => {
+  logger.error(error)
+
   return replay.status(500).send({ message: 'Internal server error' })
 })
 
