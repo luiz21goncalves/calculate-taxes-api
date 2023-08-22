012f16344d7797de012d95de73a1c8e40fce6d98
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-22 17:26:49 -0300

    chore: run lint

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import 'express-async-errors';
+import 'dotenv/config'
+import 'express-async-errors'
 
-import * as Sentry from '@sentry/node';
-import * as Tracing from '@sentry/tracing';
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
 
-import { logger } from '../utils/logger';
-import { routes } from './routes';
+import { logger } from '../utils/logger'
+import { routes } from './routes'
 
-const app = express();
+const app = express()
 
 Sentry.init({
   integrations: [
@@ -18,30 +18,30 @@ Sentry.init({
   ],
   dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
-});
+})
 
-app.use(express.json());
-app.use(cors());
-app.use(routes);
+app.use(express.json())
+app.use(cors())
+app.use(routes)
 
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
-app.use(Sentry.Handlers.errorHandler());
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.tracingHandler())
+app.use(Sentry.Handlers.errorHandler())
 
-app.get('/debug-sentry', (request, response) => {
-  throw new Error('Sentry error!');
-});
+app.get('/debug-sentry', () => {
+  throw new Error('Sentry error!')
+})
 
 app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    logger.error(err);
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    logger.error(err)
 
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
       statusCode: 500,
-    });
-  }
-);
+    })
+  },
+)
 
-app.listen(process.env.PORT, () => logger.info('Server is running'));
+app.listen(process.env.PORT, () => logger.info('Server is running'))
