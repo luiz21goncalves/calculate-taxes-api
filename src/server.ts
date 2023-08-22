8706785f0c5310f32d75b9345c8d9c918d4a13d6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-22 20:41:25 -0300

    chore: add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,24 @@
 import { app } from './app'
+import { logger } from './logger'
 
 app.listen({ port: 3333, host: '0.0.0.0' })
+
+const gracefulShutdown = () => {
+  logger.info('Shutting down server')
+  app.close(() => process.exit())
+}
+
+process.on('SIGINT', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown)
+
+process.on('uncaughtException', (error, origin) => {
+  logger.error(`uncaughtException origin ${origin}, ${error}`)
+})
+
+process.on('unhandledRejection', (error) => {
+  logger.error(`unhandledRejection, ${error}`)
+})
+
+process.on('exit', (code) => {
+  logger.info(`exit signal received with code ${code}`)
+})
