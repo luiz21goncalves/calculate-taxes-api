0b3b7c62f48af76730d2550fb78244b9ec405ff0
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-22 21:03:21 -0300

    chore: add sentry config

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,19 +2,18 @@ import 'dotenv/config'
 
 import { z } from 'zod'
 
-import { logger } from '@/logger'
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  SENTRY_DNS: z.string().url(),
+  SENTRY_DSN: z.string().url(),
 })
 
 const env = envSchema.safeParse(process.env)
 
 if (env.success === false) {
-  logger.error('Invalid environment variables.', env.error.format())
-  throw new Error('Invalid environment variables.')
+  throw new Error(
+    `Invalid environment variables. ${JSON.stringify(env.error.format())}`,
+  )
 }
 
 export const ENV = env.data
