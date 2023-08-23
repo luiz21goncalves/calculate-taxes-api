7c4633ccfda91e452e0b600341b59b52b61b33fd
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-23 08:30:17 -0300

    feat: add the route to calculate taxes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { calculateTaxes } from './controllers/calculate-taxes'
 import { parseXml } from './controllers/parse-xml'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/xml', parseXml)
+  app.get('/calculate/:id', calculateTaxes)
 }
