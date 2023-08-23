5656b374e8f7ed5a5870b4f25bb25fc7c4e74828
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-23 08:23:48 -0300

    chore: change the route to send the file

--- src/routes.ts
+++ src/routes.ts
@@ -3,5 +3,5 @@ import { FastifyInstance } from 'fastify'
 import { parseXml } from './controllers/parse-xml'
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/xml/import', parseXml)
+  app.post('/xml', parseXml)
 }
