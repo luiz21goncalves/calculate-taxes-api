46bdb65374c81af3cb3508190e8dab94e7c58753
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-23 08:01:17 -0300

    feat: add validation to save the file

--- src/controllers/parse-xml.ts
+++ src/controllers/parse-xml.ts
@@ -1,5 +1,19 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function parseXml(request: FastifyRequest, replay: FastifyReply) {
-  return replay.status(200).send({ ok: true })
+  const file = await request.file()
+
+  if (!file) {
+    return replay.status(400).send({ message: 'File is required.' })
+  }
+
+  const isXmFile = file.mimetype.includes('xml')
+
+  if (!isXmFile) {
+    return replay.status(400).send({ message: 'Send an XML file.' })
+  }
+
+  const filename = file.filename.replace('.xml', '')
+
+  return replay.status(200).send({ filename, type: file.mimetype })
 }
