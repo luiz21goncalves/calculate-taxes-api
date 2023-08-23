96241319ac7ac18511115ec6763c92a69bdf1e03
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-22 21:03:21 -0300

    chore: add sentry config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import cors from '@fastify/cors'
+import * as Sentry from '@sentry/node'
 import fastify from 'fastify'
 import healthcheck from 'fastify-healthcheck'
 
+import { ENV } from './env'
 import { logger } from './logger'
 
 const app = fastify({ logger })
+
+Sentry.init({
+  integrations: [new Sentry.Integrations.Http({ tracing: true })],
+  dsn: ENV.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  environment: ENV.NODE_ENV,
+})
 
 app.register(cors)
 app.register(healthcheck, { exposeUptime: true })
