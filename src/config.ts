518e7ddc96f46017f37265f7d68759172dd031eb
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-23 20:18:26 -0300

    fix: directories path

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
-const rootDir = join(currentDir, '../../')
+const rootDir = join(currentDir, '..')
 
-export const CONSTANTS = {
-  DIRECTORIES: {
+export const CONFIG = {
+  DIR: {
     ROOT: rootDir,
     JSON: join(rootDir, 'tmp/json'),
     XML: join(rootDir, 'tmp/xml'),
