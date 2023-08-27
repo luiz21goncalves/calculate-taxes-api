67fe148022da31c387d02f62ddb6daf760aed065
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-27 11:18:33 -0300

    fix: make sentry dsn optional

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,16 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-  SENTRY_DSN: z.string().url(),
+  SENTRY_DSN: z
+    .string()
+    .optional()
+    .refine((value) => {
+      if (value && !value.startsWith('https://')) {
+        return false
+      }
+
+      return true
+    }),
 })
 
 const env = envSchema.safeParse(process.env)
